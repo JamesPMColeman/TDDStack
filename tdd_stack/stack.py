ccca3449ae3601451274f2dfa0bd49b14402c288
James Coleman <jcolem22@msudenver.edu>
2020-04-12 17:37:17 -0600

    done with this video tutorial gonna try using circleCI again

--- tdd_stack/stack.py
+++ tdd_stack/stack.py
@@ -6,4 +6,12 @@ class Stack():
     def __len__(self):
         return len(self._storage)
 
- 
+    def push(self, item):
+        self._storage.append(item)
+
+    def pop(self):
+        try:
+            item = self._storage.pop()
+        except IndexError:
+            item = None
+        return item
